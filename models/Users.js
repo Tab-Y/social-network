cf591ce7137ca7350a2a8592391b6deb05ea4867
Tab-Y <tlyoumans@gmail.com>
2022-11-29 17:47:35 -0500

    Users model set

--- models/Users.js
+++ models/Users.js
@@ -2,11 +2,38 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
-// require content for user
-  }
-  // virtual thoughts?
-//   virtual friend count?
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please use a valid email."],    // validates email
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,        // gets associated thougts
+      ref: "Thought",
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,        // gets this users freinds
+      ref: "User",
+    }],
+  },
+  {
+    toJSON: {
+      virtuals: true,           // gets virtuals for user schema
+    },
+    id: false,
+  },
 );
+
+userSchema.virtual("friendCount").get(function () {     // sets the virtual for friends count
+  return this.friends.length;
+});
 
 
 const User = model('user', userSchema);
