0f89f177c0ef07c2235dca9c171acc7c0d2bf507
Tab-Y <tlyoumans@gmail.com>
2022-11-30 15:42:30 -0500

    thought routes set

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,10 +2,22 @@
 const router = require('express').Router();
 const { getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction } = require('../../controllers/thoughtController');    // import thought functions
 
-// GET
-// GET by _id
-// POST
-// PUT update by _id
-// DELETE remove by _id
+// GET                          
+router.route('/')               // /api/thoughts/
+    .get(getThoughts)           // calls for all thoughts
+    .post(createThought)        // creates new thought
+// GET by _id                   
+router.route('/:thoughtId')     // /api/thoughts/:thoughtId
+    .get(getSingleThought)      // calls for single thought
+    .put(updateThought)         // updates single thought
+    .delete(deleteThought)      // deletes single thought
+
+// Reactions
+router.route('/:thoughtId/reactions')               // /api/thoughts/:thoughtId/reactions
+    .post(addReaction)          // adds new reaction
+
+router.route('/:thoughtId/reactions/:reactionId')   // /api/thoughts/:thoughtId/reactions/:reactionId
+    .delete(removeReaction)     // deletes single reaction
+
 
 module.exports = router;
