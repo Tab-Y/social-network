d0c971c573d27b1807ad878c86bcaa1e4ec981c9
Tab-Y <tlyoumans@gmail.com>
2022-11-30 15:54:21 -0500

    added home route

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
 
+router.route('/')
+    .get((req, res) => {
+        return res.send(`This may be the home route, but it's not the route you're looking for!`);
+    }
+    )
+
 module.exports = router;
