6582373a44115d0ce21576a920425e6aebfdf1dc
Tab-Y <tlyoumans@gmail.com>
2022-11-30 15:49:15 -0500

    user routes set, comments seperated for ease of reading

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -2,12 +2,21 @@
 const router = require('express').Router();
 const { getUsers, getSingleUser, createUser, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/userController'); // import the exported functions
 
-// GET all users
-// get single user by _id
-// POST new user
-// PUT update user by _id
-// DELETE remove user by _id  <BONUS> remove thoughts when deleted (CASCADE DELETE)
+// GET                          
+router.route('/')                               // /api/users/
+    .get(getUsers)                              // calls for all users
+    .post(createUser)                           // creates new user
+// GET by _id                   
+router.route('/:userId')                        // /api/users/:userId
+    .get(getSingleUser)                         // calls for single user
+    .put(updateUser)                            // updates single user
+    .delete(deleteUser)                         // deletes single user
 
-// 
+// Reactions
+router.route('/:userId/friends')                // /api/users/:userId/friends
+    .post(addFriend)                            // adds new friend
+
+router.route('/:userId/friends/:friendsId')     // /api/users/:userId/friends/:friendsId
+    .delete(deleteFriend)                       // deletes single friend
 
 module.exports = router;
